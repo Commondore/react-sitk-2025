48e304b52b8bb6f1379f1ba218f85fd6e5cdc7fd
Commondore <alex.som86@gmail.com>
2025-02-05 19:56:51 +0600

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,67 @@
+import { useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/navbar/navbar";
 import { Person } from "./components/person/person";
 import { users } from "./data";
 
 const App = () => {
+  const [title, setTitle] = useState("Добро пожаловать!");
+  const [people, setPeople] = useState(users);
+  const [counter, setCounter] = useState(0);
+
+  const changeTitle = () => {
+    setTitle("Новый заголовок");
+  };
+
+  const changeName = () => {
+    const copyPeople = [...people];
+    const person = { ...copyPeople[0] };
+    person.name = "Peter Parker";
+
+    copyPeople[0] = person;
+
+    setPeople(copyPeople);
+  };
+
+  const increaseAge = () => {
+    setPeople((prevPeople) => {
+      return prevPeople.map((person) => {
+        return {
+          ...person,
+          age: person.age + 1,
+        };
+      });
+    });
+  };
+
+  const changeCounter = () => {
+    setCounter((counter) => {
+      return counter + 1;
+    });
+  };
+
   return (
     <>
       <Navbar />
+      <div className="container">
+        <h1 className="main-title" onClick={changeTitle}>
+          {title} Счетчик: {counter}
+        </h1>
+        <button className="btn" onClick={changeName}>
+          Изменить имя
+        </button>
+        <button className="btn" onClick={increaseAge}>
+          Увеличить возраст
+        </button>
+
+        <button className="btn" onClick={() => setCounter((counter) => counter + 1)}>
+          Увеличить счетчик
+        </button>
+      </div>
       <div className="wrapper">
-        <Person name={users[0].name} age={users[0].age} image={users[0].image} />
-        <Person name={users[1].name} age={users[1].age} image={users[1].image} />
-        <Person name={users[2].name} age={users[2].age} image={users[2].image} />
+        <Person name={people[0].name} age={people[0].age} image={people[0].image} />
+        <Person name={people[1].name} age={people[1].age} image={people[1].image} />
+        <Person name={people[2].name} age={people[2].age} image={people[2].image} />
       </div>
     </>
   );
